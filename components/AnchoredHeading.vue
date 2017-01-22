5ffc43d286392b1f1fc338bd914dad217e3029be
galensheen <galensheen@aliyun.com>
2017-01-22 19:10:00 +0800

    add anchoredHeading due

---
+++ components/AnchoredHeading.vue
@@ -0,0 +1,13 @@
+<script>
+  export default {
+    render(createElement) {
+      return createElement('h' + this.level, this.$slots.default);
+    },
+    props: {
+      level: {
+        type: Number,
+        required: true
+      }
+    }
+  }
+</script>
